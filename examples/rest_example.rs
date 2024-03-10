ee65d4c111b496d0a25a06d191e5323af8977b96
Maxim Gorin <onlinexmax@gmail.com>
2024-03-10 14:13:49 +0300

    Change example

--- examples/rest_example.rs
+++ examples/rest_example.rs
@@ -4,22 +4,27 @@ use monetix_connector::rest::rest_client::MonetixRestClient;
 
 #[tokio::main]
 async fn main() {
+    let project_id = std::env::var("PROJECT_ID").unwrap().parse().unwrap();
+    let secret_key = std::env::var("SECRET_KEY").unwrap();
+    let app_token = std::env::var("APP_TOKEN").unwrap();
+    let callback_url = std::env::var("CALLBACK_URL").unwrap();
+
     let client = MonetixRestClient::new(
-        0,
-        "test".to_owned(),
-        "test".to_owned(),
-        "test".to_owned(),
-        MonetixApiConfig::sandbox(),);
+        project_id,
+        secret_key,
+        app_token,
+        callback_url,
+        MonetixApiConfig::prod(),);
         
         let result = client.create_invoice_payment(
-            "",
-            "",
+            "test-payment-id-1",
+            "test-customer-id-1",
             MonetixPaymentModel {
-                amount: 0,
-                currency: "".to_string(),
-                description: None,
+                amount: 5,
+                currency: "USD".to_string(),
+                description: Some("test".into()),
                 extra_param: None,
-                best_before: "".to_string(),
+                best_before: "2050-01-01T00:00:00±00:00".to_string(),
                 moto_type: None,
                 force_method: None,
             }
