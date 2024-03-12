abfe873df92340048c0cc7baa69aa575528cc8f0
Maxim Gorin <onlinexmax@gmail.com>
2024-03-12 14:14:16 +0300

    Add sign gen for callback

--- src/rest/callback.rs
+++ src/rest/callback.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use crate::rest::models::{MonetixCreateInvoicePaymentRequest, MonetixGeneralModel};
+use crate::rest::request_signer::{MonetixRequest, MonetixSignPart};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MonetixCallBackModel {
@@ -42,8 +44,26 @@ pub struct MonetixCallbackCustomerModel {
     pub ip_address: String,
 }
 
+impl MonetixSignPart for MonetixCallbackCustomerModel {
+    fn add_sign_parts_sorted(&self, prefix: &str, targer: &mut Vec<String>) {
+        let mut parts = Vec::with_capacity(2);
+        parts.push(format!("{}:id:{}", prefix, self.id));
+        parts.push(format!("{}:ip_address:{}", prefix, self.id));
+
+        targer.append(&mut parts);
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MonetixErrorItemModel {}
+
+impl MonetixSignPart for MonetixCallbackPaymentModel {
+    fn add_sign_parts_sorted(&self, prefix: &str, targer: &mut Vec<String>) {
+        let mut parts = Vec::with_capacity(3);
+
+        targer.append(&mut parts);
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MonetixCallbackPaymentModel {
@@ -53,9 +73,60 @@ pub struct MonetixCallbackPaymentModel {
     pub payment_type: String,
 }
 
+impl MonetixSignPart for MonetixCallbackPaymentModel {
+    fn add_sign_parts_sorted(&self, prefix: &str, targer: &mut Vec<String>) {
+        let mut parts = Vec::with_capacity(3);
+        parts.push(format!("{}:id:{}", prefix, self.id));
+        parts.push(format!("{}:status:{}", prefix, self.status));
+        parts.push(format!("{}:type:{}", prefix, self.payment_type));
+
+        targer.append(&mut parts);
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MonetixRedirectDataModel {
     pub body: Option<String>,
     pub method: Option<String>,
     pub url: Option<String>,
+}
+
+impl MonetixSignPart for MonetixRedirectDataModel {
+    fn add_sign_parts_sorted(&self, prefix: &str, targer: &mut Vec<String>) {
+        let mut parts = Vec::with_capacity(3);
+
+        if let Some(body) = self.body.as_ref() {
+            parts.push(format!("{}:body:{}", prefix, body));
+        }
+
+        if let Some(method) = self.method.as_ref() {
+            parts.push(format!("{}:method:{}", prefix, method));
+        }
+
+        if let Some(url) = self.url.as_ref() {
+            parts.push(format!("{}:body:{}", prefix, url));
+        }
+
+        targer.append(&mut parts);
+    }
+}
+
+impl MonetixRequest for MonetixCallBackModel {
+    fn to_sign_string(&self) -> String {
+        let mut parts = Vec::with_capacity(100);
+
+        self.customer.add_sign_parts_sorted("customer", &mut parts);
+        self.payment.add_sign_parts_sorted("payment", &mut parts);
+
+        if let Some(redirect_data) = self.redirect_data.as_ref() {
+            redirect_data.add_sign_parts_sorted("redirect_data", &mut parts);
+        }
+
+        if let Some(errors) = self.errors.as_ref() {
+            errors.add_sign_parts_sorted("errors", &mut parts);
+        }
+
+        parts.sort();
+        parts.join(";")
+    }
 }
