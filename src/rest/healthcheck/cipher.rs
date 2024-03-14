47b983ade13d79ea9785eceb4f03fc3101bbb434
Maxim Gorin <onlinexmax@gmail.com>
2024-03-14 17:53:31 +0300

    Add decrypt method

--- src/rest/healthcheck/cipher.rs
+++ src/rest/healthcheck/cipher.rs
@@ -19,12 +19,24 @@ impl MonetixHealthcheckCipher {
 impl MonetixHealthcheckCipher {
     pub fn encrypt(&self, data: &str) -> Result<String, String> {
         let iv = MonetixHealthcheckCipher::get_iv();
-        let encrypted_data = MonetixHealthcheckCipher::encrypt_inner(data.as_bytes(), &self.secret_key, &iv);
+        let encrypted_data = MonetixHealthcheckCipher::encrypt_with_iv(data.as_bytes(), &self.secret_key, &iv);
         let encoded_data = MonetixHealthcheckCipher::encode(&encrypted_data);
         let encoded_iv = MonetixHealthcheckCipher::encode(&iv);
         let result = format!("{}::{}", encoded_data, encoded_iv);
 
         Ok(MonetixHealthcheckCipher::encode(result.as_bytes()))
+    }
+
+    pub fn decrypt(&self, data: &str) -> Result<String, String> {
+        let decoded = MonetixHealthcheckCipher::decode(data.as_bytes())?;
+        let data = std::str::from_utf8(&decoded).unwrap();
+        let splits: Vec<&str> = data.split("::").collect();
+        let data = MonetixHealthcheckCipher::decode(splits[0].as_bytes()).unwrap();
+        let iv = MonetixHealthcheckCipher::decode(splits[1].as_bytes()).unwrap();
+        let decrypted = MonetixHealthcheckCipher::decrypt_with_iv(&data, &self.secret_key, &iv).unwrap();
+        let data = std::str::from_utf8(&decrypted).unwrap();
+
+        Ok(data.to_string())
     }
 
     fn encode(data: &[u8]) -> String {
@@ -33,7 +45,17 @@ impl MonetixHealthcheckCipher {
         base64_encoded.to_owned()
     }
 
-    fn encrypt_inner(data: &[u8], key: &str, iv: &[u8]) -> Vec<u8> {
+    fn decode(data: &[u8]) -> Result<Vec<u8>, String> {
+        let base64_decoded = general_purpose::STANDARD.decode(data);
+
+        let Ok(base64_decoded) = base64_decoded else {
+            return Err(format!("{}", base64_decoded.unwrap_err()));
+        };
+
+        Ok(base64_decoded)
+    }
+
+    fn encrypt_with_iv(data: &[u8], key: &str, iv: &[u8]) -> Vec<u8> {
         let mut aes_key = [0; 32];
 
         for (i, byte) in key.as_bytes().iter().enumerate() {
@@ -43,6 +65,19 @@ impl MonetixHealthcheckCipher {
         let cipher = Cipher::new_256(&aes_key);
 
         cipher.cbc_encrypt(iv, data)
+    }
+
+    fn decrypt_with_iv(src: &[u8], key: &str, iv: &[u8]) -> Result<Vec<u8>, String> {
+        let mut aes_key = [0; 32];
+
+        for (i, byte) in key.as_bytes().iter().enumerate() {
+            aes_key[i] = *byte;
+        }
+
+        let cipher = Cipher::new_256(&aes_key);
+        let decrypted = cipher.cbc_decrypt(iv, src);
+
+        Ok(decrypted)
     }
 
     fn get_iv() -> Vec<u8> {
@@ -63,8 +98,8 @@ mod tests {
         let cipher = MonetixHealthcheckCipher {
             secret_key: "secret".to_string(),
         };
-        let _result = cipher.encrypt(data).unwrap();
-
-        //assert_eq!(result, "cDROL0FqVC9wck1keWhrWmZ3Zm9EQ3NJQmZQKzNpYVdMb2dDbHU4bmJEUUc5TGVweVJoOTl2UXQ3NXFnZk9ZS2FUaHgzQzVnS081K0s4b3IvUDQ0VVhXNzlCLzFDL1BJQnYxZFdFQWU4RTNZK2hZbFdsbGtHN1pkRG82ZXlUb1FjOVpMVGQxVjQ5S0ZJYVZiY3BFcng4ZHB5ZG9YRnFBeElwckxzQnUyT1hDeEIzYlVqekVLRHV1dExRdXU3aUNHdWlEMUJiMzVDeHhiRktseGgraG5uSjFqY2U2RGg3YjVRY0M1b0RWc2VLSGhveDh0NEIrUk16a0NtM3BhWFhmZ3hMUytXeS9vcUxYcnZPS1pBQ3hEb2hBZXJTTjdRY0pISjczaDNWS0VQbUFyM2JFdnRyRVVrWkl6bG9oSVNsSDlBdEJqMFMyTlBCeG4xYnJEZjA1NUNGMHF5d3JtVjZaWTRsQkZHTWFrVU1ONmhZSTU0a0hzb1hXNUJ3QSt6b2x4aERCOENUUXZQMGgrRDBMSDZtODB1dk9hb1VwMDVOZWl5QWZYZjZpS0pqbkRLMlpYK3lkSUU0NW1zRGY2VDN4VVhTZDBJOVEwSnlqK3ZGNEdZT3ZWV3FpUm02NW5hWTlmZ0xwWWpmWXZXMFBMWlpoMSt1SmVkK3hmZ1h3QzVJNjY4cGlUajVQWHlzcVBEWFNJcVUzdjVkT0x4OVByWWRsMDVqcG9jTnJqUFc0Y0tueHplUmF4S0NNU3hxaG1FOUdhVWtCT0t3M0tSUHo1SzZQR1F1bVgxQT09OjpaVEl5WVRBeE5HSTFabVpoTkRreU1BPT0");
+        let result = cipher.encrypt(data).unwrap();
+        
+        assert_eq!(data, cipher.decrypt(&result).unwrap());
     }
 }
